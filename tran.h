a589e1dabc8790e3374581abcd3a2d5c030b82f0
zhixiangnuli <fanwenqiang@mail.ustc.edu.cn>
2025-12-27 23:10:14 +0800

    已编写完成所有算法代码，测试3x3x3棋盘正方体网格算例得到的结果非常好

--- tran.h
+++ tran.h
@@ -8071,6 +8071,96 @@ void _get_Qx(const Ktensor *perm, const double *verts, int divn, int &Iter, doub
     //                 MPFA(i, j, k, Axis::ZPOSITIVE);
     //         }
     for (int k = 0; k <= nz; k++)
+        for (int j = 0; j < ny; j++)
+            for (int i = 0; i <= nx; i++)
+                if (i == 0 || i == nx || k == 0 || k == nz)
+                {
+                    MPFA(i, j, k, Axis::YPOSITIVE);
+                    MPFA(i, j + 1, k, Axis::YNEGATIVE);
+                }
+                else
+                {
+                    int idx[4];
+                    idx[0] = (k - 1) * nx * ny + j * nx + i - 1;
+                    idx[1] = (k - 1) * nx * ny + j * nx + i;
+                    idx[2] = k * nx * ny + j * nx + i;
+                    idx[3] = k * nx * ny + j * nx + i - 1;
+                    // 柱坐标，l,r,angle
+                    double new_o[3], new_x[3], new_y[3], new_z[3];
+                    _get_midpoint(&verts1(k, j, i, 0, 0), &verts1(k, j, i, 2, 0), new_o);
+                    _get_normalized(&verts1(k, j, i, 0, 0), &verts1(k, j, i, 2, 0), new_y);
+                    double pc[4][3];
+                    _get_centroid(&verts1(k - 1, j, i, 0, 0), &verts1(k - 1, j, i, 2, 0), &verts1(k - 1, j, i, 4, 0), &verts1(k - 1, j, i, 6, 0), pc[0]);
+                    _get_centroid(&verts1(k, j, i, 0, 0), &verts1(k, j, i, 1, 0), &verts1(k, j, i, 2, 0), &verts1(k, j, i, 3, 0), pc[1]);
+                    _get_centroid(&verts1(k, j, i, 0, 0), &verts1(k, j, i, 2, 0), &verts1(k, j, i, 4, 0), &verts1(k, j, i, 6, 0), pc[2]);
+                    _get_centroid(&verts1(k, j, i - 1, 0, 0), &verts1(k, j, i - 1, 1, 0), &verts1(k, j, i - 1, 2, 0), &verts1(k, j, i - 1, 3, 0), pc[3]);
+                    // 求l->求r
+                    double l[4];
+                    double r[4], r_p[4];
+                    double temp[3];
+                    for (int q = 0; q < 4; ++q)
+                    {
+                        _vectorize(new_o, pc[q], temp);
+                        l[q] = _dot_product(temp, new_y);
+                        r[q] = std::sqrt(_l2norm(temp) * _l2norm(temp) - l[q] * l[q]);
+                    }
+                    // 先求新坐标轴，再求angle
+                    _vectorize(new_o, pc[3], new_x);
+                    _cross_product(new_y, new_x, new_z);
+                    _get_normalized(new_z);
+                    _cross_product(new_y, new_z, new_x);
+                    double angle[4], angle_p[4];
+                    angle[3] = Pi;
+                    for (int q = 0; q < 3; ++q)
+                    {
+                        _vectorize(new_o, pc[q], temp);
+                        _get_angle(new_x, new_z, temp, r[q], angle[q]);
+                    }
+                    // 渗透率张量变换，变换矩阵为正交矩阵
+                    Eigen::Matrix3d J, matK[4];
+                    J << new_x[0], new_x[1], new_x[2],
+                        new_y[0], new_y[1], new_y[2],
+                        new_z[0], new_z[1], new_z[2];
+                    Eigen::RowVector3d p_o;
+                    p_o << _cx(k - 1, j, i - 1) - new_o[0], _cy(k - 1, j, i - 1) - new_o[1], _cz(k - 1, j, i - 1) - new_o[2];
+                    p_o *= J.transpose();
+                    r_p[0] = std::sqrt(p_o(0) * p_o(0) + p_o(2) * p_o(2));
+                    _get_angle(p_o[0], p_o[2], r_p[0], angle_p[0]);
+                    p_o << _cx(k - 1, j, i) - new_o[0], _cy(k - 1, j, i) - new_o[1], _cz(k - 1, j, i) - new_o[2];
+                    p_o *= J.transpose();
+                    r_p[1] = std::sqrt(p_o(0) * p_o(0) + p_o(2) * p_o(2));
+                    _get_angle(p_o[0], p_o[2], r_p[1], angle_p[1]);
+                    p_o << _cx(k, j, i) - new_o[0], _cy(k, j, i) - new_o[1], _cz(k, j, i) - new_o[2];
+                    p_o *= J.transpose();
+                    r_p[2] = std::sqrt(p_o(0) * p_o(0) + p_o(2) * p_o(2));
+                    _get_angle(p_o[0], p_o[2], r_p[2], angle_p[2]);
+                    p_o << _cx(k, j, i - 1) - new_o[0], _cy(k, j, i - 1) - new_o[1], _cz(k, j, i - 1) - new_o[2];
+                    p_o *= J.transpose();
+                    r_p[3] = std::sqrt(p_o(0) * p_o(0) + p_o(2) * p_o(2));
+                    _get_angle(p_o[0], p_o[2], r_p[3], angle_p[3]);
+                    for (int q = 0; q < 4; ++q)
+                    {
+                        _get_matK(pem1[idx[q]], matK[q]);
+                        matK[q] = J * matK[q] * J.transpose();
+                    }
+                    double alpha = 0.0;
+                    Eigen::Matrix2d K[4];
+                    for (int q = 0; q < 4; ++q)
+                    {
+                        K[q](0, 0) = matK[q](0, 0);
+                        K[q](0, 1) = matK[q](0, 2);
+                        K[q](1, 0) = matK[q](2, 0);
+                        K[q](1, 1) = matK[q](2, 2);
+                    }
+
+                    FAM(idx, _get_distance(&verts1(k, j, i, 0, 0), &verts1(k, j, i, 2, 0)), l, r, angle, K, alpha, r_p, angle_p);
+                    if (alpha == 0.0 || alpha > 0.99)
+                    {
+                        MPFA(i, j, k, Axis::YPOSITIVE);
+                        MPFA(i, j + 1, k, Axis::YNEGATIVE);
+                    }
+                }
+    for (int k = 0; k <= nz; k++)
         for (int j = 0; j <= ny; j++)
             for (int i = 0; i < nx; i++)
                 if (j == 0 || j == ny || k == 0 || k == nz)
@@ -8110,7 +8200,7 @@ void _get_Qx(const Ktensor *perm, const double *verts, int divn, int &Iter, doub
                     _get_normalized(new_z);
                     _cross_product(new_z, new_x, new_y);
                     double angle[4], angle_p[4];
-                    angle[3] = -Pi;
+                    angle[3] = Pi;
                     for (int q = 0; q < 3; ++q)
                     {
                         _vectorize(new_o, pc[q], temp);
@@ -8152,6 +8242,90 @@ void _get_Qx(const Ktensor *perm, const double *verts, int divn, int &Iter, doub
                     {
                         MPFA(i, j, k, Axis::XPOSITIVE);
                         MPFA(i + 1, j, k, Axis::XNEGATIVE);
+                    }
+                }
+    for (int k = 0; k < nz; k++)
+        for (int j = 0; j <= ny; j++)
+            for (int i = 0; i <= nx; i++)
+                if (j == 0 || j == ny || i == 0 || i == nx)
+                {
+                    MPFA(i, j, k, Axis::ZPOSITIVE);
+                    MPFA(i, j, k + 1, Axis::ZNEGATIVE);
+                }
+                else
+                {
+                    int idx[4];
+                    idx[0] = k * nx * ny + (j - 1) * nx + i - 1;
+                    idx[1] = k * nx * ny + (j - 1) * nx + i;
+                    idx[2] = k * nx * ny + j * nx + i;
+                    idx[3] = k * nx * ny + j * nx + i - 1;
+                    // 柱坐标，l,r,angle
+                    double new_o[3], new_x[3], new_y[3], new_z[3];
+                    _get_midpoint(&verts1(k, j, i, 0, 0), &verts1(k, j, i, 4, 0), new_o);
+                    _get_normalized(&verts1(k, j, i, 0, 0), &verts1(k, j, i, 4, 0), new_z);
+                    double pc[4][3];
+                    _get_centroid(&verts1(k, j - 1, i, 0, 0), &verts1(k, j - 1, i, 2, 0), &verts1(k, j - 1, i, 4, 0), &verts1(k, j - 1, i, 6, 0), pc[0]);
+                    _get_centroid(&verts1(k, j, i, 0, 0), &verts1(k, j, i, 1, 0), &verts1(k, j, i, 4, 0), &verts1(k, j, i, 5, 0), pc[1]);
+                    _get_centroid(&verts1(k, j, i, 0, 0), &verts1(k, j, i, 2, 0), &verts1(k, j, i, 4, 0), &verts1(k, j, i, 6, 0), pc[2]);
+                    _get_centroid(&verts1(k, j, i - 1, 0, 0), &verts1(k, j, i - 1, 1, 0), &verts1(k, j, i - 1, 4, 0), &verts1(k, j, i - 1, 5, 0), pc[3]);
+                    // 求l->求r
+                    double l[4];
+                    double r[4], r_p[4];
+                    double temp[3];
+                    for (int q = 0; q < 4; ++q)
+                    {
+                        _vectorize(new_o, pc[q], temp);
+                        l[q] = _dot_product(temp, new_z);
+                        r[q] = std::sqrt(_l2norm(temp) * _l2norm(temp) - l[q] * l[q]);
+                    }
+                    // 先求新坐标轴，再求angle
+                    _vectorize(new_o, pc[3], new_x);
+                    _cross_product(new_x, new_z, new_y);
+                    _get_normalized(new_y);
+                    _cross_product(new_y, new_z, new_x);
+                    double angle[4], angle_p[4];
+                    angle[3] = Pi;
+                    for (int q = 0; q < 3; ++q)
+                    {
+                        _vectorize(new_o, pc[q], temp);
+                        _get_angle(new_x, new_y, temp, r[q], angle[q]);
+                    }
+                    // 渗透率张量变换，变换矩阵为正交矩阵
+                    Eigen::Matrix3d J, matK[4];
+                    J << new_x[0], new_x[1], new_x[2],
+                        new_y[0], new_y[1], new_y[2],
+                        new_z[0], new_z[1], new_z[2];
+                    Eigen::RowVector3d p_o;
+                    p_o << _cx(k, j - 1, i - 1) - new_o[0], _cy(k, j - 1, i - 1) - new_o[1], _cz(k, j - 1, i - 1) - new_o[2];
+                    p_o *= J.transpose();
+                    r_p[0] = std::sqrt(p_o(0) * p_o(0) + p_o(1) * p_o(1));
+                    _get_angle(p_o[0], p_o[1], r_p[0], angle_p[0]);
+                    p_o << _cx(k, j - 1, i) - new_o[0], _cy(k, j - 1, i) - new_o[1], _cz(k, j - 1, i) - new_o[2];
+                    p_o *= J.transpose();
+                    r_p[1] = std::sqrt(p_o(0) * p_o(0) + p_o(1) * p_o(1));
+                    _get_angle(p_o[0], p_o[1], r_p[1], angle_p[1]);
+                    p_o << _cx(k, j, i) - new_o[0], _cy(k, j, i) - new_o[1], _cz(k, j, i) - new_o[2];
+                    p_o *= J.transpose();
+                    r_p[2] = std::sqrt(p_o(0) * p_o(0) + p_o(1) * p_o(1));
+                    _get_angle(p_o[0], p_o[1], r_p[2], angle_p[2]);
+                    p_o << _cx(k, j, i - 1) - new_o[0], _cy(k, j, i - 1) - new_o[1], _cz(k, j, i - 1) - new_o[2];
+                    p_o *= J.transpose();
+                    r_p[3] = std::sqrt(p_o(0) * p_o(0) + p_o(1) * p_o(1));
+                    _get_angle(p_o[0], p_o[1], r_p[3], angle_p[3]);
+                    for (int q = 0; q < 4; ++q)
+                    {
+                        _get_matK(pem1[idx[q]], matK[q]);
+                        matK[q] = J * matK[q] * J.transpose();
+                    }
+                    double alpha = 0.0;
+                    Eigen::Matrix2d K[4];
+                    for (int q = 0; q < 4; ++q)
+                        K[q] = matK[q].block(0, 0, 2, 2);
+                    FAM(idx, _get_distance(&verts1(k, j, i, 0, 0), &verts1(k, j, i, 4, 0)), l, r, angle, K, alpha, r_p, angle_p);
+                    if (alpha == 0.0 || alpha > 0.99)
+                    {
+                        MPFA(i, j, k, Axis::ZPOSITIVE);
+                        MPFA(i, j, k + 1, Axis::ZNEGATIVE);
                     }
                 }
     pmgmres_ilu_cr(nx * ny * nz, nnz, Ptr, Idx, Val, p, B, 1000, 1000, 1e-5, 1e-5);
